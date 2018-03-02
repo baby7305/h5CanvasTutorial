066b07fc3588d5bbf17e5514cea5d66bda319fc5
baby7305 <songhaibei7305@163.com>
2018-03-02 17:02:13 +0800

    模式，重复图形填充测试

--- js/test.js
+++ js/test.js
@@ -2,22 +2,18 @@
 window.onload = function () {
     var drawing = document.getElementById("drawing"),
         btn = document.getElementById("export-btn");
-        // <img id="smiley" src="images/smile.gif" border="1" title="Image tag" />
+    // <img id="smiley" src="images/smile.gif" border="1" title="Image tag" />
 
     //make sure <canvas> is completely supported
-    if (drawing.getContext){
-            
-        var context = drawing.getContext("2d");
-        var image = document.getElementById("smiley");
-        
-        //draw regular size
-        context.drawImage(image, 10, 10);
-        
-        //draw smaller
-        context.drawImage(image, 50, 10, 20, 30);
+    if (drawing.getContext) {
 
-        //draw just part of the image
-        context.drawImage(image, 0, 10, 50, 50, 0, 100, 40, 60);
+        var context = drawing.getContext("2d"),
+            image = document.images[0],
+            pattern = context.createPattern(image, "repeat");
+
+        //draw a rectangle
+        context.fillStyle = pattern;
+        context.fillRect(10, 10, 150, 150);
 
     }
 
