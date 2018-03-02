f6433c5d06d47f0c2989540153a6e90e721c03a3
baby7305 <songhaibei7305@163.com>
2018-03-02 12:00:13 +0800

    绘制矩形描边测试

--- js/test.js
+++ js/test.js
@@ -9,12 +9,12 @@ window.onload = function () {
         var context = drawing.getContext("2d");
 
         //draw a red rectangle
-        context.fillStyle = "#ff0000";
-        context.fillRect(10, 10, 50, 50);
-
+        context.strokeStyle = "#ff0000";
+        context.strokeRect(10, 10, 50, 50);
+    
         //draw a blue rectangle that's semi-transparent
-        context.fillStyle = "rgba(0,0,255,0.5)";
-        context.fillRect(30, 30, 50, 50);
+        context.strokeStyle = "rgba(0,0,255,0.5)";
+        context.strokeRect(30, 30, 50, 50);
     }
 
     btn.onclick = function () {
