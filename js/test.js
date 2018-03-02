68fcf70cdcaa9b72e716cadbd00ec98836c5ced2
baby7305 <songhaibei7305@163.com>
2018-03-02 14:01:11 +0800

    文本起点对齐和终点对齐测试

--- js/test.js
+++ js/test.js
@@ -30,10 +30,22 @@ window.onload = function () {
         
         //add some text - not supported by all browsers
         if (context.strokeText){
+                    
+            //normal
             context.font = "bold 14px Arial";
             context.textAlign = "center";
             context.textBaseline = "middle";
             context.fillText("12", 100, 20);
+            
+            //start-aligned
+            context.textAlign = "start";
+            context.fillText("12", 100, 40);
+            
+            //start-aligned
+            context.textAlign = "end";
+            context.fillText("12", 100, 60);
+            
+            
         } else {
             alert("Your browser doesn't support the canvas text API.");
         }
