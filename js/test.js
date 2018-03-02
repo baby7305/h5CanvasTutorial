bf2ebe160adb9d579b529d71c77a01635033fc26
baby7305 <songhaibei7305@163.com>
2018-03-02 15:00:02 +0800

    绘图保存笔触，原点测试

--- js/test.js
+++ js/test.js
@@ -8,13 +8,23 @@ window.onload = function () {
 
         var context = drawing.getContext("2d");
 
-        //draw a red rectangle
         context.fillStyle = "#ff0000";
-        context.fillRect(10, 10, 50, 50);
+        context.save();
 
-        //draw a blue rectangle that's semi-transparent
-        context.fillStyle = "rgba(0,0,255,0.5)";
-        context.fillRect(30, 30, 50, 50);
+        context.fillStyle = "#00ff00";
+        context.translate(100, 100);
+        context.save();
+
+        context.fillStyle = "#0000ff";
+        context.fillRect(0, 0, 100, 200);   //draws blue rectangle at (100, 100)
+        
+        context.restore();
+        context.fillRect(10, 10, 100, 200);   //draws green rectangle at (110, 110)
+
+        context.restore();
+        context.fillRect(0, 0, 100, 200);    //draws red rectangle at (0,0)
+
+
     }
 
     btn.onclick = function () {
