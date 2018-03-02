92aa2bd331b2a1eec5cc67e919c21e4ea4471342
baby7305 <songhaibei7305@163.com>
2018-03-02 15:33:07 +0800

    绘制图像测试

--- js/test.js
+++ js/test.js
@@ -2,19 +2,23 @@
 window.onload = function () {
     var drawing = document.getElementById("drawing"),
         btn = document.getElementById("export-btn");
+        // <img id="smiley" src="images/smile.gif" border="1" title="Image tag" />
 
     //make sure <canvas> is completely supported
-    if (drawing.getContext) {
-
+    if (drawing.getContext){
+            
         var context = drawing.getContext("2d");
+        var image = document.getElementById("smiley");
+        
+        //draw regular size
+        context.drawImage(image, 10, 10);
+        
+        //draw smaller
+        context.drawImage(image, 50, 10, 20, 30);
 
-        //draw a red rectangle
-        context.fillStyle = "#ff0000";
-        context.fillRect(10, 10, 50, 50);
+        //draw just part of the image
+        context.drawImage(image, 0, 10, 50, 50, 0, 100, 40, 60);
 
-        //draw a blue rectangle that's semi-transparent
-        context.fillStyle = "rgba(0,0,255,0.5)";
-        context.fillRect(30, 30, 50, 50);
     }
 
     btn.onclick = function () {
