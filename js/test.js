ddb292f55a714be45e6d702bce9f69649809bfd5
baby7305 <songhaibei7305@163.com>
2018-03-02 15:39:03 +0800

    绘制图形阴影测试

--- js/test.js
+++ js/test.js
@@ -8,16 +8,19 @@ window.onload = function () {
 
         var context = drawing.getContext("2d");
 
+        //setup shadow
+        context.shadowOffsetX = 5;
+        context.shadowOffsetY = 5;
+        context.shadowBlur = 4;
+        context.shadowColor = "rgba(0, 0, 0, 0.5)";
+
         //draw a red rectangle
         context.fillStyle = "#ff0000";
         context.fillRect(10, 10, 50, 50);
-    
-        //draw a blue rectangle that's semi-transparent
-        context.fillStyle = "rgba(0,0,255,0.5)";
+
+        //draw a blue rectangle
+        context.fillStyle = "rgba(0,0,255,1)";
         context.fillRect(30, 30, 50, 50);
-        
-        //clear a rectangle that overlaps both of the previous rectangles
-        context.clearRect(40, 40, 10, 10);
     }
 
     btn.onclick = function () {
