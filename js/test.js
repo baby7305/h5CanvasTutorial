6c3a07ee047f2af2cd2707aa944cda0436f25ec7
baby7305 <songhaibei7305@163.com>
2018-03-02 17:08:32 +0800

    绘制图形边框阴影测试

--- js/test.js
+++ js/test.js
@@ -6,20 +6,21 @@ window.onload = function () {
     //make sure <canvas> is completely supported
     if (drawing.getContext) {
 
-        var context = drawing.getContext("2d"),
-            gradient = context.createRadialGradient(55, 55, 10, 55, 55, 30);
+        var context = drawing.getContext("2d");
 
-
-        gradient.addColorStop(0, "white");
-        gradient.addColorStop(1, "black");
+        //setup shadow
+        context.shadowOffsetX = 5;
+        context.shadowOffsetY = 5;
+        context.shadowBlur = 4;
+        context.shadowColor = "rgba(0, 0, 0, 0.5)";
 
         //draw a red rectangle
-        context.fillStyle = "#ff0000";
-        context.fillRect(10, 10, 50, 50);
+        context.strokeStyle = "#ff0000";
+        context.strokeRect(10, 10, 50, 50);
 
-        //draw a gradient rectangle
-        context.fillStyle = gradient;
-        context.fillRect(30, 30, 50, 50);
+        //draw a blue rectangle that's semi-transparent
+        context.strokeStyle = "rgba(0,0,255,0.5)";
+        context.strokeRect(30, 30, 50, 50);
     }
 
     btn.onclick = function () {
