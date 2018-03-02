010f0efd94c99e0af99ade1f9bc7185518fdc4be
baby7305 <songhaibei7305@163.com>
2018-03-02 14:38:21 +0800

    变换原点，旋转表针测试

--- js/test.js
+++ js/test.js
@@ -10,45 +10,28 @@ window.onload = function () {
 
         //start the path
         context.beginPath();
-                
+
         //draw outer circle
         context.arc(100, 100, 99, 0, 2 * Math.PI, false);
-        
+
         //draw inner circle
         context.moveTo(194, 100);
         context.arc(100, 100, 94, 0, 2 * Math.PI, false);
-        
-        //draw hour hand
-        context.moveTo(100,100);
-        context.lineTo(100, 15);
-        
+
+        //move center to (100,100)
+        context.translate(100, 100);
+        context.rotate(1);
+
         //draw minute hand
-        context.moveTo(100, 100);
-        context.lineTo(35, 100);
-        
+        context.moveTo(0, 0);
+        context.lineTo(0, -85);
+
+        //draw hour hand
+        context.moveTo(0, 0);
+        context.lineTo(-65, 0);
+
         context.stroke();
-        
-        //add some text - not supported by all browsers
-        if (context.strokeText){
-                    
-            //normal
-            context.font = "bold 14px Arial";
-            context.textAlign = "center";
-            context.textBaseline = "middle";
-            context.fillText("12", 100, 20);
-            
-            //start-aligned
-            context.textAlign = "start";
-            context.fillText("12", 100, 40);
-            
-            //start-aligned
-            context.textAlign = "end";
-            context.fillText("12", 100, 60);
-            
-            
-        } else {
-            alert("Your browser doesn't support the canvas text API.");
-        }
+
     }
 
     btn.onclick = function () {
