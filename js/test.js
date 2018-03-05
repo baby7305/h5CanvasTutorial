b12806967364bdc2155dc9a2917045f465799746
baby7305 <songhaibei7305@163.com>
2018-03-05 16:30:51 +0800

    不生效代码

--- js/test.js
+++ js/test.js
@@ -6,10 +6,26 @@ function pageLoaded() {
     var context = canvas.getContext("2d");
 
     //Our drawing code here
-    
-    
-    
+    var audio1 = document.getElementById("audio");
+    var mp3Support, oggSupport;
 
+    if (audio1.canPlayType) {
+        mp3Support = "" !== audio1.canPlayType("audio/mpeg");
+        oggSupport = "" !== audio1.canPlayType("audio/ogg;codecs=\"vorbis\"");
+    } else {
+        mp3Support = false;
+        oggSupport = false;
+    }
+
+    var soundFileExtn = oggSupport ? ".ogg" : mp3Support ? ".mp3" : undefined;
+    if (soundFileExtn) {
+        var sound = new Audio();
+
+        sound.addEventListener("canplaythrough", function () {
+            sound.play();
+        })
+        sound.src = "audios/bullet1" + soundFileExtn;
+    }
 
 
 
