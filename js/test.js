5bd12aad3cdd935e0b1c256334168c522b042d92
baby7305 <songhaibei7305@163.com>
2018-03-05 16:52:55 +0800

    图片加载完成提示

--- js/test.js
+++ js/test.js
@@ -6,31 +6,10 @@ function pageLoaded() {
     var context = canvas.getContext("2d");
 
     //Our drawing code here
-    var audio1 = document.getElementById("audio");
-    var mp3Support, oggSupport;
+    var image = new Image();
+    image.src = "images/spaceship.png";
 
-    if (audio1.canPlayType) {
-        mp3Support = "" !== audio1.canPlayType("audio/mpeg");
-        oggSupport = "" !== audio1.canPlayType("audio/ogg;codecs=\"vorbis\"");
-    } else {
-        mp3Support = false;
-        oggSupport = false;
-    }
-
-    var soundFileExtn = oggSupport ? ".ogg" : mp3Support ? ".mp3" : undefined;
-    if (soundFileExtn) {
-        var sound = new Audio();
-
-        sound.addEventListener("canplaythrough", function () {
-            sound.play();
-        })
-        sound.src = "audios/bullet1" + soundFileExtn;
-    }
-
-
-
-
-
-
-
+    image.onload = function () {
+        alert("Image finished loading");
+    };
 }
