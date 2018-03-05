45efdf7fb7846da5cc0817c64cce76fcab386175
baby7305 <songhaibei7305@163.com>
2018-03-05 10:13:08 +0800

    canvas画布初始化测试

--- js/test.js
+++ js/test.js
@@ -1,3 +1,10 @@
-function pageLoaded(){
-	alert("hello world");
+function pageLoaded() {
+    //Get a handle to the canvas project
+    var canvas = document.getElementById("testcanvas");
+
+    //Get the 2d context for this canvas
+    var context = canvas.getContext("2d");
+
+    //Our drawing code here
+
 }
