4721eaeabb216c5b0a1e46d7f89eb8d93bef3234
baby7305 <songhaibei7305@163.com>
2018-03-05 11:30:16 +0800

    路径绘图之复杂多边形测试

--- js/test.js
+++ js/test.js
@@ -6,11 +6,39 @@ function pageLoaded() {
     var context = canvas.getContext("2d");
 
     //Our drawing code here
-    context.fillRect(200, 10, 100, 100);
-    context.fillRect(50, 70, 90, 30);
-    context.strokeRect(110, 10, 50, 50);
-    context.strokeRect(30, 10, 50, 50);
-    context.clearRect(210, 20, 30, 20);
-    context.clearRect(260, 20, 30, 20);
+    context.beginPath();
+    context.moveTo(10, 120);
+    context.lineTo(10, 180);
+    context.lineTo(110, 150);
+    context.closePath();
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(140, 160);
+    context.lineTo(140, 220);
+    context.lineTo(40, 190);
+    context.closePath();
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(160, 160);
+    context.lineTo(170, 220);
+    context.lineTo(240, 210);
+    context.lineTo(260, 170);
+    context.lineTo(190, 140);
+    context.closePath();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(100, 300, 40, 0, Math.PI, true);
+    context.stroke();
+
+    context.beginPath();
+    context.arc(100, 300, 30, 0, 2 * Math.PI, true);
+    context.fill();
+
+    context.beginPath();
+    context.arc(300, 300, 25, 0, 3 / 2 * Math.PI, false);
+    context.stroke();
 
 }
