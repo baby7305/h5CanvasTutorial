8a88001b1efa1a1d703ccbe1e2d8d1722d6e4248
baby7305 <songhaibei7305@163.com>
2018-03-05 14:35:53 +0800

    图像绘制测试

--- js/test.js
+++ js/test.js
@@ -6,23 +6,12 @@ function pageLoaded() {
     var context = canvas.getContext("2d");
 
     //Our drawing code here
-    context.fillStyle = "red";
-    context.fillRect(310, 160, 100, 50);
+    var image = document.getElementById("spaceship");
+    context.drawImage(image, 0, 350);
+    context.drawImage(image, 0, 400, 100, 25);
+    context.drawImage(image, 0, 0, 60, 50, 0, 420, 60, 50);
+    
 
-    context.strokeStyle = "green";
-    context.strokeRect(310, 240, 100, 50);
-
-    context.fillStyle = "rgb(255,255,0)";
-    context.fillRect(420, 160, 100, 50);
-
-    context.fillStyle = "rgba(0,255,0,0.6)";
-    context.fillRect(450, 180, 100, 50);
-
-    var fireImage = document.getElementById("fire");
-    var pattern = context.createPattern(fireImage, "repeat");
-
-    context.fillStyle = pattern;
-    context.fillRect(420, 240, 200, 200);
 
 
 
