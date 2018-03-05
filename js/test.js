42433ec96e391d52a3692dc95742ade733e761b1
baby7305 <songhaibei7305@163.com>
2018-03-05 15:25:23 +0800

    图像的缩放与旋转绘制测试

--- js/test.js
+++ js/test.js
@@ -10,7 +10,23 @@ function pageLoaded() {
     context.drawImage(image, 0, 350);
     context.drawImage(image, 0, 400, 100, 25);
     context.drawImage(image, 0, 0, 60, 50, 0, 420, 60, 50);
-    
+
+    context.translate(250, 370);
+    context.rotate(Math.PI / 3);
+    context.drawImage(image, 0, 0, 60, 50, -30, -25, 60, 50);
+
+    context.rotate(-Math.PI / 3);
+    context.translate(-240, -370);
+
+    context.translate(300, 370);
+    context.rotate(3 * Math.PI / 4);
+    context.drawImage(image, 0, 0, 60, 50, -30, -25, 60, 50);
+
+    context.rotate(-3 * Math.PI / 4);
+    context.translate(-300, -370);
+
+
+
 
 
 
