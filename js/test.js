844277aea2c8828a4e5bcb2181564c53c6d8710b
baby7305 <songhaibei7305@163.com>
2018-03-05 13:26:44 +0800

    文本绘制测试

--- js/test.js
+++ js/test.js
@@ -6,39 +6,10 @@ function pageLoaded() {
     var context = canvas.getContext("2d");
 
     //Our drawing code here
-    context.beginPath();
-    context.moveTo(10, 120);
-    context.lineTo(10, 180);
-    context.lineTo(110, 150);
-    context.closePath();
-    context.fill();
-
-    context.beginPath();
-    context.moveTo(140, 160);
-    context.lineTo(140, 220);
-    context.lineTo(40, 190);
-    context.closePath();
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(160, 160);
-    context.lineTo(170, 220);
-    context.lineTo(240, 210);
-    context.lineTo(260, 170);
-    context.lineTo(190, 140);
-    context.closePath();
-    context.stroke();
-
-    context.beginPath();
-    context.arc(100, 300, 40, 0, Math.PI, true);
-    context.stroke();
-
-    context.beginPath();
-    context.arc(100, 300, 30, 0, 2 * Math.PI, true);
-    context.fill();
-
-    context.beginPath();
-    context.arc(300, 300, 25, 0, 3 / 2 * Math.PI, false);
-    context.stroke();
+    context.fillText("This is some text...", 330, 40);
+    context.font = "10pt Arial";
+    context.fillText("This is in 10pt Arial", 330, 60);
+    context.font = "16pt Arial";
+    context.strokeText("This is stroked in 16pt Arial....", 330, 80);
 
 }
