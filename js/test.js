4210de7182b3d42ec8216313276f663a02794290
baby7305 <songhaibei7305@163.com>
2018-03-05 10:28:50 +0800

    填充、描边、清除矩形画布测试

--- js/test.js
+++ js/test.js
@@ -6,5 +6,11 @@ function pageLoaded() {
     var context = canvas.getContext("2d");
 
     //Our drawing code here
+    context.fillRect(200, 10, 100, 100);
+    context.fillRect(50, 70, 90, 30);
+    context.strokeRect(110, 10, 50, 50);
+    context.strokeRect(30, 10, 50, 50);
+    context.clearRect(210, 20, 30, 20);
+    context.clearRect(260, 20, 30, 20);
 
 }
