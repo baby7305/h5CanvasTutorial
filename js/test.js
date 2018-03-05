5871f6383c0a81ee4c56c413993a922c3b1498d3
baby7305 <songhaibei7305@163.com>
2018-03-05 14:17:06 +0800

    笔触绘制测试

--- js/test.js
+++ js/test.js
@@ -6,10 +6,25 @@ function pageLoaded() {
     var context = canvas.getContext("2d");
 
     //Our drawing code here
-    context.fillText("This is some text...", 330, 40);
-    context.font = "10pt Arial";
-    context.fillText("This is in 10pt Arial", 330, 60);
-    context.font = "16pt Arial";
-    context.strokeText("This is stroked in 16pt Arial....", 330, 80);
+    context.fillStyle = "red";
+    context.fillRect(310, 160, 100, 50);
+
+    context.strokeStyle = "green";
+    context.strokeRect(310, 240, 100, 50);
+
+    context.fillStyle = "rgb(255,255,0)";
+    context.fillRect(420, 160, 100, 50);
+
+    context.fillStyle = "rgba(0,255,0,0.6)";
+    context.fillRect(450, 180, 100, 50);
+
+    var fireImage = document.getElementById("fire");
+    var pattern = context.createPattern(fireImage, "repeat");
+
+    context.fillStyle = pattern;
+    context.fillRect(420, 240, 200, 200);
+
+
+
 
 }
