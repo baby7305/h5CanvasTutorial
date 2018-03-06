c42393ec23827da1f91b8c337c514a87a193d3a3
baby7305 <songhaibei7305@163.com>
2018-03-06 10:22:49 +0800

    设置脚本初始化启动界面

--- js/game.js
+++ js/game.js
@@ -1,3 +1,33 @@
-function pageLoaded(){
-	alert("hello world");
+var game = {
+	init: function () {
+		game.canvas = document.getElementById("gamecanvas");
+		game.context = document.getElementById("2d");
+		game.hideScreens();
+		game.showScreen("gamestartscreen");
+
+	},
+
+	hideScreens: function () {
+		var screens = document.getElementsByClassName("gameplayer");
+		for (let i = screens.length - 1; i >= 0; i--) {
+			var screen = screens[i];
+			screen.style.display = "none";
+
+		}
+	},
+
+	hideScreen: function (id) {
+		var screen = document.getElementById(id);
+		screen.style.display = "none";
+	},
+
+	showScreen: function (id) {
+		var screen = document.getElementById(id);
+		screen.style.display = "block";
+	},
+
 }
+
+window.addEventListener("load", function () {
+	game.init();
+});
